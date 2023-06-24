1d70812a8060455468bfd5a45ad9f04003a43ca8
Nathan Stepper <nstepper@uw.edu>
2023-06-23 21:53:32 -0700

    had to make a new repo since the old one is really messed up

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# 03-FancyFrance
-A password generator site that allows users to generate a secure and custom password.
+# FancyFrance
+
+A simple html website that allows users to generate a secure password for all secure password needs.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Demo](#demo)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+- [License](#license)
+## Installation
+
+1. Clone the repo using the command: 
+
+-git clone https://github.com/nstepper/03-fancy-france.git  
+
+2. Just open the index.html file in your browser and voila!
+
+## Usage
+
+It can be used for anything that needs a secure password. There is some options to give more control over the password length and complexity.
+
+## Demo
+  
+![Alt Text](assets/03-FancyFrance.gif)
+
+## Contributing
+
+Feel free to sumbit a pull request for review
+
+## Questions
+
+Feel free to sumbit any questions to nstepper@outlook.com
+
+## License
+This project is licensed under the MIT license. [![License](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)
---
+++ assets/script.js
@@ -0,0 +1,46 @@
+function generatePassword() {
+    var passwordLength = document.querySelector("#passwordLength").value;
+    var includeSpecial = document.querySelector("#includeSpecial").checked;
+    var includeUpper = document.querySelector("#includeUpper").checked;
+    var includeLower = document.querySelector("#includeLower").checked;
+    var password = "";
+    var charset = "";
+    
+  
+    if (includeSpecial) {
+      charset += "!@#$%^&*()";
+    }
+    if (includeUpper) {
+      charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (includeLower) {
+      charset += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (charset === "") {
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    }
+  
+    for (var i = 0, n = charset.length; i < passwordLength; ++i) {
+      password += charset.charAt(Math.floor(Math.random() * n));
+    }
+  
+    return password;
+  }
+  
+  // Get references to the form elements
+  var generateBtn = document.querySelector("#generate");
+  var passwordLength = document.querySelector("#passwordLength");
+  var includeSpecial = document.querySelector("#includeSpecial");
+  var includeUpper = document.querySelector("#includeUpper");
+  var includeLower = document.querySelector("#includeLower");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
